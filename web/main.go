612778a6cbae451943cad0aa60230c171c6e42d4
Александр Кузнецов <spocher102@outlook.com>
2023-06-14 12:54:43 +0300

    Сделал пути к уонфигу и db

--- web/main.go
+++ web/main.go
@@ -40,7 +40,7 @@ var log waLog.Logger
 var logLevel = "INFO"
 var debugLogs = flag.Bool("debug", true, "Enable debug logs?")
 var dbDialect = flag.String("db-dialect", "sqlite3", "Database dialect (sqlite3 or postgres)")
-var dbAddress = flag.String("db-address", "file:./web/mdtest.db?_foreign_keys=on", "Database address")
+var dbAddress = flag.String("db-address", "file:mdtest.db?_foreign_keys=on", "Database address")
 var requestFullSync = flag.Bool("request-full-sync", false, "Request full (1 year) history sync when logging in?")
 var pairRejectChan = make(chan bool, 1)
 var historySyncID int32
@@ -50,7 +50,7 @@ var startupTime = time.Now().Unix()
 func main() {
 
 	// считываем файл кофигурации
-	content, err := os.ReadFile("./web/config.json")
+	content, err := os.ReadFile("config.json")
 
 	// если есть ошибка
 	if err != nil {
