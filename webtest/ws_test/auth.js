78773ecea747946c1c8e5345a60cd0b7102e96e6
Александр Кузнецов <spocher102@outlook.com>
2023-08-31 16:53:41 +0300

    auth.js

--- webtest/ws_test/auth.js
+++ webtest/ws_test/auth.js
@@ -48,7 +48,7 @@ class InstanceAuthorization {
 		this.loaderButton.setDisable();
 
 		//устанавливаем сокет соединение
-		this.webSocket = new WebSocket(`ws://test.apisender.com/ws?proxy=5.101.35.4:8000:M1Y1DR:2BY0ZH&app_secret=af15ede2bbad492e776f12b1b2ff2c2bbc544018`);
+		this.webSocket = new WebSocket(`ws://158.160.12.106:10003/ws?proxy=85.195.81.143:13754:FpCz2p:RU2qUo&app_secret=af15ede2bbad492e776f12b1b2ff2c2bbc544018`);
 
 		//открытие соединения
 		this.webSocket.onopen = () => {
